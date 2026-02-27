da1c01ea489dc8142023881c9ef0cda22dff339c
daksh <daksh.dangi@gmail.com>
2026-02-26 22:08:13 -0500

    initial commit: portfilio setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://daksh-dangi.github.io',
   base: '/effective-broccoli',
   integrations: [tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
