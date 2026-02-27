d785e2462f3cd26740e632cfe6ddf59269bcf67c
daksh <daksh.dangi@gmail.com>
2026-02-26 22:24:51 -0500

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/postcss';
 
 export default defineConfig({
   site: 'https://daksh-dangi.github.io',
   base: '/effective-broccoli',
   integrations: [tailwind()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    },
+  },
 });
